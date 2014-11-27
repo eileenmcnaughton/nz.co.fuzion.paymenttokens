b3e1ef64be796d28af58f6ab3d9b93551b2634c8
Eileen McNaughton <eileen@fuzion.co.nz>
2014-11-27 13:55:38 +1300

    improvements to query api

--- api/v3/PaymentToken/Query.php
+++ api/v3/PaymentToken/Query.php
@@ -13,25 +13,40 @@
  */
 function civicrm_api3_payment_token_query($params) {
   $token = CRM_Core_DAO::executeQuery(
-    "SELECT r.id, r.payment_processor_id, r.processor_id, t.id as token_id FROM civicrm_contribution_recur r LEFT JOIN civicrm_payment_token t ON t.code= r.processor_id
+    "SELECT r.id as contribution_recur_id, r.payment_processor_id, r.processor_id, t.id as token_id, r.contact_id
+     FROM civicrm_contribution_recur r LEFT JOIN civicrm_payment_token t ON t.code= r.processor_id
      WHERE t.expiry_date IS NULL
-     AND r.processor_id IS NOT NULL"
+     AND r.processor_id IS NOT NULL
+     AND r.payment_processor_id IS NOT NULL"
   );
   $result = array();
   while ($token->fetch()) {
     try {
-      $processorTypeID = civicrm_api3('payment_processor', 'getvalue', array('id' => $token->payment_processor_id, 'return' => 'payment_processor_type_id'));
-      $processor = civicrm_api3('payment_processor_type', 'getvalue', array('id' => $processorTypeID, 'return' => 'name'));
-      $query = civicrm_api3($processor, 'tokenquery', array('contribution_recur_id' => $token->id, 'sequential' => 1));
-      $result[$token->id] = $query['values'][0];
-      if (!empty($existingToken['values'][0])) {
-        $token = array_merge($existingToken['values'][0], $token);
+      $tokenParams = (array) $token;
+      $processorTypeID = civicrm_api3('payment_processor', 'getvalue', array(
+          'id' => $token->payment_processor_id,
+          'return' => 'payment_processor_type_id'
+        ));
+      $processor = civicrm_api3('payment_processor_type', 'getvalue', array(
+          'id' => $processorTypeID,
+          'return' => 'name'
+        ));
+      $query = civicrm_api3($processor, 'tokenquery', array(
+          'contribution_recur_id' => $token->contribution_recur_id,
+          'sequential' => 1
+        ));
+      $result[$token->contribution_recur_id] = $query['values'][0];
+      if (!empty($query['values'][0])) {
+        $tokenParams = array_merge($tokenParams, $query['values'][0]);
       }
-      $tokenCreate = civicrm_api3('payment_token', 'create', $token);
     }
     catch (Exception $e) {
-      //continue
+      $result[$token->contribution_recur_id] = array('message' => $e->getMessage());
     }
+
+    $tokenCreate = civicrm_api3('payment_token', 'create', $tokenParams);
+    $result[$token->contribution_recur_id]['token_id'] = $tokenCreate['id'];
+
   }
   return civicrm_api3_create_success($result, $params);
 }
