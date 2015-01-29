c320bf8e9722d4e1043af7faed853b228266bab0
Eileen McNaughton <eileen@fuzion.co.nz>
2015-01-30 10:25:28 +1300

    code cleanup to new standards

--- CRM/Payment/BAO/PaymentToken.php
+++ CRM/Payment/BAO/PaymentToken.php
@@ -5,18 +5,24 @@
  * Date: 19/11/2014
  * Time: 8:48 PM
  */
-class CRM_Payment_BAO_PaymentToken extends CRM_Payment_DAO_PaymentToken
-{
-    static function create($params)
-    {
-        $hook = empty($params['id']) ? 'create' : 'edit';
+class CRM_Payment_BAO_PaymentToken extends CRM_Payment_DAO_PaymentToken {
 
-        CRM_Utils_Hook::pre($hook, 'PaymentToken', CRM_Utils_Array::value('id', $params), $params);
-        $instance = new CRM_Payment_DAO_PaymentToken();
-        $instance->copyValues($params);
-        $instance->save();
-        CRM_Utils_Hook::post($hook, 'PaymentToken', $instance->id, $instance);
+  /**
+   * Create payment token record.
+   *
+   * @param $params
+   *
+   * @return \CRM_Payment_DAO_PaymentToken
+   */
+  public  static function create($params) {
+    $hook = empty($params['id']) ? 'create' : 'edit';
 
-        return $instance;
-    }
+    CRM_Utils_Hook::pre($hook, 'PaymentToken', CRM_Utils_Array::value('id', $params), $params);
+    $instance = new CRM_Payment_DAO_PaymentToken();
+    $instance->copyValues($params);
+    $instance->save();
+    CRM_Utils_Hook::post($hook, 'PaymentToken', $instance->id, $instance);
+
+    return $instance;
+  }
 }
