85d9f94c733b65fc236a5447f0fd18cd21386fc3
Eileen McNaughton <eileen@fuzion.co.nz>
2015-06-23 14:46:40 +1200

    Query tokens with expiries 6-weeks into the future for sites monitoring upcoming expiries

--- api/v3/PaymentToken/Query.php
+++ api/v3/PaymentToken/Query.php
@@ -15,7 +15,7 @@ function civicrm_api3_payment_token_query($params) {
   $token = CRM_Core_DAO::executeQuery(
     "SELECT r.id as contribution_recur_id, r.payment_processor_id, r.processor_id, t.id as token_id, r.contact_id
      FROM civicrm_contribution_recur r LEFT JOIN civicrm_payment_token t ON t.code= r.processor_id
-     WHERE t.expiry_date IS NULL
+     WHERE (t.expiry_date IS NULL OR t.expiry_date < DATE_ADD(NOW(), INTERVAL 6 WEEK))
      AND r.processor_id IS NOT NULL
      AND r.payment_processor_id IS NOT NULL"
   );
@@ -25,7 +25,7 @@ function civicrm_api3_payment_token_query($params) {
       $tokenParams = array(
         'contribution_recur_id' => $token->contribution_recur_id,
         'payment_processor_id' => $token->payment_processor_id,
-        'token_id' => $token->token_id,
+        'id' => $token->token_id,
       );
       $processorTypeID = civicrm_api3('payment_processor', 'getvalue', array(
           'id' => $token->payment_processor_id,
@@ -44,6 +44,7 @@ function civicrm_api3_payment_token_query($params) {
       if (!empty($query['values'][0])) {
         $tokenParams = array_merge($tokenParams, $query['values'][0]);
       }
+
       $tokenCreate = civicrm_api3('payment_token', 'create', $tokenParams);
       $result[$token->contribution_recur_id]['token_id'] = $tokenCreate['id'];
     }
